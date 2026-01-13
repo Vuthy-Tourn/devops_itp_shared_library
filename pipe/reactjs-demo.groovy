e706f6a10a649b9209077f227cd0c38b315440e8
Vuthy_Tourn <vuthytuon168@gmail.com>
2026-01-13 11:37:41 +0700

    Modified: dockerfile of reactjs

---
+++ pipe/reactjs-demo.groovy
@@ -0,0 +1,81 @@
+@Library("itp_shared_lib@main") _
+
+pipeline {
+    agent any
+
+    environment {
+        REPO_NAME='vuthytourn'
+        IMAGE_NAME='jenkins-itp-reactjs'
+        TAG='latest'
+        CHAT_ID='777493627'
+        CHAT_TOKEN='8477902807:AAGdQkcWqEb-gtJ19Efup07VDjSeMOIuWB4'
+    }
+
+    stages {
+
+        stage('Clone Code') {
+            steps {
+                git 'https://github.com/Vuthy-Tourn/reactjs-fullstack-template.git'
+            }
+        }
+
+        stage('Check Dockerfile') {
+            steps {
+                script {
+                    if (!fileExists('Dockerfile')) {
+                        echo 'Dockerfile not found, loading from Shared Library...'
+                        def dockerfile = libraryResource 'reactjs/dev.Dockerfile'
+                        writeFile file: 'Dockerfile', text: dockerfile
+                    } else {
+                        echo 'Dockerfile found in project'
+                    }
+                }
+            }
+        }
+
+        stage('Build Image') {
+            steps {
+                sh '''
+                docker build -t ${REPO_NAME}/${IMAGE_NAME}:${TAG} .
+                '''
+            }
+        }
+
+        stage('Push Image') {
+            steps {
+                withCredentials([usernamePassword(
+                    credentialsId: 'DOCKERHUB-CREDENTIAL',
+                    usernameVariable: 'DH_USERNAME',
+                    passwordVariable: 'DH_PASSWORD'
+                )]) {
+                    sh '''
+                    echo "$DH_PASSWORD" | docker login -u "$DH_USERNAME" --password-stdin
+                    docker push ${REPO_NAME}/${IMAGE_NAME}:${TAG}
+                    '''
+                }
+            }
+        }
+
+        stage('Run Service') {
+            steps {
+                sh '''
+                docker stop react-app || true
+                docker rm react-app || true
+                docker run -dp 3000:8080 --name react-app ${REPO_NAME}/${IMAGE_NAME}:${TAG}
+                '''
+            }
+        }
+
+        stage('Send Message') {
+            steps {
+                script {
+                    sendTelegramMessage(
+                        "Deploy Successfully 🟢",
+                        CHAT_TOKEN,
+                        CHAT_ID
+                    )
+                }
+            }
+        }
+    }
+}
--- resources/reactjs/dev.Dockerfile
+++ resources/reactjs/dev.Dockerfile
@@ -1,29 +1,14 @@
-# Use official Node.js image as the base image
-FROM node:latest as build
-
-# Set working directory in the container
+FROM node:20-alpine AS build
 WORKDIR /app
 
-# Copy package.json and package-lock.json to container
 COPY package*.json ./
-
-# Install dependencies
 RUN npm install
 
-# Copy all files from the current directory to the container
 COPY . .
-
-# Build React app
 RUN npm run build
 
-# Use Nginx image as the base image for serving the React app
-FROM nginx:latest
+FROM nginx:alpine
+COPY --from=build /app/dist /usr/share/nginx/html
 
-# Copy build files from previous stage to Nginx server's html directory
-COPY --from=build /app/build /usr/share/nginx/html
-
-# Expose port 80 to the outside world
 EXPOSE 80
-
-# Command to run Nginx
 CMD ["nginx", "-g", "daemon off;"]
